70c2c506fe219766dc1689ce388674289ce4b0a5
Ralica Popova <ralicaterzieva90@gmail.com>
2024-11-17 17:12:34 +0200

    Add more side effects logic

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,10 +1,26 @@
 import { useContext, useEffect, useState } from 'react';
 import { Box, Text } from '@chakra-ui/react';
 import CartContext from './../lib/context/Cart/index';
+import getProductsById from './../lib/graphql/queries/getProductsById';
+import graphql from '@/lib/graphql';
 
 export default function Cart() {
     const { items } = useContext(CartContext);
-    
+    const [products, setProducts] = useState([]);
+    const hasProducts = Object.keys(item).length;
+
+    useEffect(() => {
+        if(!hasProducts) return;
+
+        graphql.request(getProductsById, {
+            ids: Object.keys(items),
+        })
+        .then((data) => {
+            setProducts(data.products)
+        })
+        .catch((err) => console.log(err))
+    }, [JSON.stringify(products)])
+
     return (
         <Box
         rounded="xl"
